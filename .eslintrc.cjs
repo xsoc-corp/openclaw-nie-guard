dd04e0051a7618f6b09ad7153c49db2435c61b32
XSOC Dev <dev@xsoccorp.com>
2026-04-20 23:30:11 -0700

    Phase 1u: commit ESLint varsIgnorePattern for underscore-prefixed unused vars

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     '@typescript-eslint/no-explicit-any': 'warn',
     'no-console': 'off'
   },
